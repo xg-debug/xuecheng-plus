6d901f77b1917f60918634e37022bb4e07beca29
xg-debug <83619900+xg-debug@users.noreply.github.com>
2025-03-07 15:24:42 +0800

    测试使用Spring Security进行认证授权的过程

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.xuecheng.ucenter.mapper.XcUserMapper;
 import com.xuecheng.ucenter.model.po.XcUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,11 +31,13 @@ public class LoginController {
     }
 
     @RequestMapping("/r/r1")
+    @PreAuthorize("hasAuthority('p1')")//拥有p1权限方可访问
     public String r1() {
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
+    @PreAuthorize("hasAuthority('p2')")//拥有p2权限方可访问
     public String r2() {
         return "访问r2资源";
     }
---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/config/TokenConfig.java
@@ -0,0 +1,42 @@
+package com.xuecheng.content.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * 令牌策略配置类
+ **/
+@Configuration
+public class TokenConfig {
+
+    //jwt签名密钥,与认证服务保持一致
+    private String SIGNING_KEY = "mq123";
+
+    @Bean
+    public TokenStore tokenStore() {
+        //JWT令牌存储方案
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
+   /* @Bean
+    public TokenStore tokenStore() {
+        //使用内存存储令牌（普通令牌）
+        return new InMemoryTokenStore();
+    }*/
+
+
+}
